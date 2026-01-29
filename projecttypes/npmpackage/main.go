66d3eaf240a4c62f1a9754e2805de54b471a6533
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-30 00:49:13 +0300

    `projecttypes/npmpackage/Create()` simplified.

--- projecttypes/npmpackage/main.go
+++ projecttypes/npmpackage/main.go
@@ -1,13 +1,26 @@
 package npmpackage
 
 import (
+	"errors"
+	"os"
+
 	"github.com/robinbobin/create-project/utils"
 )
 
 func Create() bool {
 	defer utils.RecoverFromPanic()
 
-	initPackage()
+	utils.RunCmd("npm init")
+
+	_, err := os.Stat("package.json")
+
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+
+	utils.PanicOnError(err)
+	utils.UsePNPM()
+	utils.AskSortJSON("package.json")
 
 	return true
 }
