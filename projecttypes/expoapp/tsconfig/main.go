2752ee6f3d5b5b195b730e3983f5c09f260cfcad
Ruben Shalimov <r_shalimov@inbox.ru>
2026-03-20 00:48:01 +0300

    `projecttypes/expoapp/tsconfig`: logic retrofitted.

--- projecttypes/expoapp/tsconfig/main.go
+++ projecttypes/expoapp/tsconfig/main.go
@@ -1,21 +1,12 @@
 package tsconfig
 
 import (
-	"github.com/robinbobin/create-project/assets"
 	"github.com/robinbobin/create-project/utils"
 )
 
 const tsconfig_json = "tsconfig.json"
 
 func Process(isProjectReset bool) {
-	if !utils.Confirm("Would you like to use a custom tsconfig?", true) {
-		return
-	}
-
-	assets.CopyFile("tsconfig.base.json", tsconfig_json)
-
-	backup()
-
 	tsconfig := utils.ReadJSON(tsconfig_json)
 
 	deleteCompilerOptions(isProjectReset, tsconfig)
--- projecttypes/expoapp/tsconfig/process_extends.go
+++ projecttypes/expoapp/tsconfig/process_extends.go
@@ -2,9 +2,21 @@ package tsconfig
 
 import (
 	"fmt"
+	"path/filepath"
+
+	"github.com/robinbobin/create-project/assets"
+	"github.com/robinbobin/create-project/utils"
 )
 
 func processExtends(tsconfig map[string]any) {
+	if !utils.Confirm("Would you like to add a custom base tsconfig?", true) {
+		return
+	}
+
+	const baseFile = "tsconfig.base"
+
+	assets.CopyFile(fmt.Sprintf("%v.json", baseFile), tsconfig_json)
+
 	extends := []string{}
 
 	switch ext := tsconfig["extends"].(type) {
@@ -20,7 +32,7 @@ func processExtends(tsconfig map[string]any) {
 		panic(fmt.Errorf("\"%v\": \"extends\" is of type \"%T\", equals \"%v\" and can't be parsed", tsconfig_json, ext, ext))
 	}
 
-	extends = append(extends, "./tsconfig.base")
+	extends = append(extends, fmt.Sprintf(".%c%v", filepath.Separator, baseFile))
 
 	tsconfig["extends"] = extends
 }
