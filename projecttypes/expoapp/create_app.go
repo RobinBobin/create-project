8e2932ee561a2b0dd7a7dcf87a8a40503729d275
Ruben Shalimov <r_shalimov@inbox.ru>
2026-04-20 15:06:09 +0300

    `projecttypes/expoapp/getTemplateVersion()` added.

--- projecttypes/expoapp/create_app.go
+++ projecttypes/expoapp/create_app.go
@@ -2,6 +2,7 @@ package expoapp
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 
 	"github.com/robinbobin/create-project/utils"
@@ -13,7 +14,7 @@ func createApp() (appName string, mustApproveBuilds bool) {
 	approveBuildsRe := regexp.MustCompile(`Run "pnpm approve-builds" to pick which dependencies should be allowed to run scripts\.`)
 
 	utils.CaptureCmdOutput(&utils.CaptureCmdOutputOptions{
-		CmdWithArgs: "pnpm create expo-app",
+		CmdWithArgs: fmt.Sprintf("pnpm create expo-app --template default@%v", getTemplateVersion()),
 		CapturedOutputProcessor: func(strippedOutput string) (needsMoreStdin bool) {
 			matches := appNameRe.FindStringSubmatch(strippedOutput)
 
---
+++ projecttypes/expoapp/template_version.go
@@ -0,0 +1,47 @@
+package expoapp
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/charmbracelet/huh"
+	"github.com/robinbobin/create-project/utils"
+)
+
+func getTemplateVersion() string {
+	ar := []string{"expo-template-default", "expo"}
+
+	stdout := strings.Builder{}
+
+	for index, packageName := range ar {
+		stdout.Reset()
+
+		utils.CaptureCmdOutput(&utils.CaptureCmdOutputOptions{
+			CmdWithArgs: fmt.Sprintf("pnpm show %v dist-tags.latest", packageName),
+			PreProcessCmd: func(cmd *exec.Cmd) {
+				cmd.Env = append(os.Environ(), "npm_config_progress=false")
+			},
+			Stdout: &stdout,
+		})
+
+		ar[index] = strings.Split(stdout.String(), ".")[0]
+	}
+
+	if ar[0] == ar[1] {
+		return "latest"
+	}
+
+	version := ar[1]
+
+	utils.PanicOnError(
+		huh.NewSelect[string]().
+			Title("Which Expo version would you like to use?").
+			Options(huh.NewOptions(ar...)...).
+			Value(&version).
+			Run(),
+	)
+
+	return fmt.Sprint("sdk-", version)
+}
