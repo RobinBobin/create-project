597c567cbeff13a6d6a3585386f63e5616cef951
Ruben Shalimov <r_shalimov@inbox.ru>
2026-08-04 23:53:40 +0300

    fix(assets): fix bug in ESLint rule options

--- assets/assets/eslint/ruleOptions/index.js
+++ assets/assets/eslint/ruleOptions/index.js
@@ -1,4 +1,3 @@
-export { importX } from './importX/index.js'
 export { js } from './js/index.js'
 export { simpleImportSort } from './simpleImportSort/index.js'
 export { stylistic } from './stylistic/index.js'
